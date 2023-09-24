504c2ff7ddb5a2aa6bc576993448066ea5fb0e6b
VinceBarnachea <barnacheatolentino@example.com>
2023-09-24 11:43:40 +0800

    Contact Added

--- js/script.js
+++ js/script.js
@@ -1,3 +1,16 @@
 $(document).ready(function() {
-    console.log('Jquery Ready');
+    // Calculate the total height of the document
+    var docHeight = $(document).height();
+    
+    // Bind the scroll event to the window
+    $(window).scroll(function() {
+        // Calculate the current scroll position
+        var scrollTop = $(window).scrollTop();
+        
+        // Calculate the scroll percentage
+        var scrollPercent = (scrollTop / (docHeight - $(window).height())) * 100;
+        
+        // Update the progress bar width
+        $('.progress-bar').css('width', scrollPercent + '%');
+    });
 });
